580c5a6a34ab7c79530e213642cf08b0ec0dc376
koto <raymondkoto23@gmail.com>
2022-12-13 15:48:59 +0700

    Update View Category

---
+++ src/components/utilities/CardCategory.js
@@ -0,0 +1,20 @@
+import { Link } from "react-router-dom";
+
+function CardCategory(props){
+
+    return (
+        <div className="col-6 col-md-3 mb-4" key={props.id}>
+            <Link to={`/category /${props.slug}`} className="text-decoration-none text-dark">
+                <div className="card h-100 border-0 rounded shadow-sm">
+                    <div className="card-body text-center">
+                        <img src={props.image} style={{ width: "80px" }} alt=""/>
+                        <hr />
+                        <h6>{props.name.toUpperCase()}</h6>
+                    </div>
+                </div>
+            </Link>
+        </div>
+    )
+}
+
+export default CardCategory;
--- src/pages/web/home/Index.js
+++ src/pages/web/home/Index.js
@@ -1,18 +1,79 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 
 import LayoutWeb from "../../../layouts/Web";
 
 import Slider from '../../../components/web/Slider';
 
+import Api from "../../../api";
+
+import CardCategory from "../../../components/utilities/CardCategory";
+
 function Home() {
+
+    //title
+    document.title = "HIDDEN GEMS - WEBSITE KOTO";
+    
+    //state categories
+    const [categories, setCategories] = useState([]);
+
+    //function fetchData categories
+    const fetchDataCategories = async () => {
+        //fetching rest api
+        await Api.get('/api/web/categories')
+        .then((response) => {
+            
+            //set data to state
+            setCategories(response.data.data)
+        })
+    }
+
+    //hook
+    useEffect(() => {
+
+        //call fetchdata categories
+        fetchDataCategories();
+
+    }, []);
+
     return (
         <React.Fragment>
             <LayoutWeb>
 
                 <Slider />
 
+                <div className="container mb-5">
+                    <div className="row mt-minus-87">
+                        <div className="col-md-12">
+                            <div className="card border-0 rounded shadow-sm">
+                                <div className="card-body">
+                                    <h5>
+                                        <i className="fa fa-search"></i> Find Your Healing Place!
+                                    </h5>
+                                    <p>
+                                        Find a Staycation Place With Your Friends!
+                                    </p>
+                                    <hr />
+                                    <input type="text" className="form-control" placeholder="Find Your Destination Here!"/>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="row justify-content-center mt-4">
+                        {
+                            categories.map((category) => (
+                                <CardCategory
+                                key={category.id}
+                                name={category.name}
+                                slug={category.slug}
+                                image={category.image}
+                                />
+                            ))
+                        }
+                    </div>
+                </div>
+
                 <div className="container mt-5">
-                    <h1> Home </h1>
+                    <h1> Content </h1>
                 </div>
             </LayoutWeb>
         </React.Fragment>
