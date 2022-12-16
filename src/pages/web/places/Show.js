3d0efb9306429191d0d52cb0a278370ca02351d2
koto <raymondkoto23@gmail.com>
2022-12-16 16:06:40 +0700

    Update Detail Place

--- src/pages/web/places/Show.js
+++ src/pages/web/places/Show.js
@@ -24,7 +24,7 @@ function WebPlacesShow() {
             setPlace(response.data.data);
 
             //document tittle
-            document.title = `${response.data.data.data.title} - Website Healing`
+            document.title = `${response.data.data.title} - Website Healing`
         });
     };
 
@@ -40,8 +40,75 @@ function WebPlacesShow() {
         <React.Fragment>
             <LayoutWeb>
                 <div className="container mt-80">
-                    Halaman Places Show
-                </div>
+                    <div className="row">
+                        <div className="col-md-7 mb-4">
+                            <div className="card border-0 rounded shadow-sm">
+                                <div className="card-body">
+                                    <h4>{place.title}</h4>
+                                    <span className="card-text">
+                                        <i className="fa fa-map-marker"> <i> {place.address}</i></i>
+                                    </span>
+                                    <hr />
+                                    <div dangerouslySetInnerHTML={{ __html: place.description }} 
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="col-md-5 mb-4">
+                            <div className="card border-0 rounded shadow-sm">
+                                <div className="card-body">
+                                    <h5>
+                                        <i className="fa fa-map-marked-alt"></i> MAPS
+                                    </h5>
+                                    <hr />
+                                </div>
+                                <hr />
+                                <div className="card-body">
+                                    <div className="row">
+                                        <div className="col-md-2 col-2">
+                                            <div className="icon-info-green">
+                                                <i className="fa fa-map-marker-alt"></i>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-10 col-10">
+                                            <div className="capt-info fw-bold">ADDRESS</div>
+                                            <div className="sub-title-info"><i>{place.address}</i></div>
+                                        </div>
+                                        <div className="col-md-2 col-2">
+                                            <div className="icon-info-green">
+                                                <i className="fa fa-clock"></i>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-10 col-10">
+                                            <div className="capt-info fw-bold">OFFICE HOURS</div>
+                                            <div className="sub-title-info">{place.office_hours}</div>
+                                        </div>
+                                        <div className="col-md-2 col-2">
+                                            <div className="icon-info-green">
+                                                <i className="fa fa-phone"></i>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-10 col-10">
+                                            <div className="capt-info fw-bold">PHONE</div>
+                                            <div className="sub-title-info">{place.phone}</div>
+                                        </div>
+                                        <div className="col-md-2 col-2">
+                                            <div className="icon-info-green">
+                                                <i className="fa fa-globe-asia"></i>
+                                            </div>
+                                        </div>
+                                        <div className="col-md-10 col-10">
+                                            <div className="capt-info fw-bold">WEBSITE</div>
+                                            <div className="sub-title-info">
+                                                <a href={place.website} className="text-decoration-none">{place.website}</a>
+                                            </div>
+                                        </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
             </LayoutWeb>
         </React.Fragment>
     )
